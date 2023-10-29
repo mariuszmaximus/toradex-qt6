297050f098e04b32a9d4d7c408e2d0cc9d5f4e91
mariuszmaximus <mariusz.maximus@gmail.com>
2023-10-29 12:29:07 +0000

    add current revision

--- step002.sh
+++ step002.sh
@@ -2,7 +2,9 @@ export PATH=~/bin:$PATH
 
 mkdir ./oe-core
  cd ./oe-core
-# repo init -u git://git.toradex.com/toradex-manifest.git -b dunfell-5.x.y -m tdxref/default.xml
+
+# 10/26/2023 3bd1cf97414290b11c8b079c926b6197053068bc 
+
  repo init -u git://git.toradex.com/toradex-manifest.git -b kirkstone-6.x.y -m tdxref/default.xml
  repo sync
 
